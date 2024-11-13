8213b3df607e1f348eff27819e66a7a7e547c2b7
subbb_in <subin8350@gmail.com>
2024-11-13 16:03:31 +0900

    edit test

---
+++ src/test/java/com/springboot_aws/repositoryTest.java
@@ -0,0 +1,17 @@
+package com.springboot_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class repositoryTest {
+
+    @Test
+    public void testInsert(){
+        int a = 3;
+
+        assertEquals(a, 5);
+    }
+}
